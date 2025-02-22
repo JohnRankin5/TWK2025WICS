005c246770ccb9cf138d91bc67a46471df2f1341
JohnRankin5 <147775648+JohnRankin5@users.noreply.github.com>
2025-02-22 14:06:05 -0500

    Updated App

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.JSONField(
+        default=list,
+        help_text="List of ingredients as a JSON array (e.g., ['2 eggs', '1 cup sugar'])."
+    )
+    directions = models.JSONField(
+        default=list,
+        help_text="List of steps, each containing 'Active' and 'Passive' keys."
+    )
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
